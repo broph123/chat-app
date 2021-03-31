fadf07321a8e9a1dac04bb4d792c0d01419c0601
Henry Brophy <henrybrophy@henrys-mbp.lan>
2021-03-31 09:03:43 -0400

    About to begin conversation context.

--- chat/src/components/App.js
+++ chat/src/components/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Login from './Login'
+import Dashboard from './Dashboard'
+
 import useLocalStorage from '../hooks/useLocalStorage'
+import { ContactsProvider } from '../context/ContactsProvider'
 
 function App() {
 
   const [id,setId] = useLocalStorage('id')
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id}/>
+    </ContactsProvider>
+  )
   return (
-    <>
-    {id}
-    <Login onIdSubmit={setId}/>
-    </>
+    id ? dashboard :<Login onIdSubmit={setId}/>
+    
   );
 }
 
