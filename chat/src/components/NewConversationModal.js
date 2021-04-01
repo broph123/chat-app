cf9f63a1bd1c685382d42c0ac6fff59a69bf179d
Henry Brophy <henrybrophy@henrys-mbp.lan>
2021-04-01 07:31:36 -0400

    Test Commit

--- chat/src/components/NewConversationModal.js
+++ chat/src/components/NewConversationModal.js
@@ -10,7 +10,7 @@ function NewConversationModal({closeModal}) {
 
     function handleSubmit(e){
         e.preventDefault()
-        // createConversation(selectedContactIds)
+        createConversation(selectedContactIds)
         closeModal()
         
     }
